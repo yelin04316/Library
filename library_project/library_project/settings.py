3f41d71b8704811c7c3673aab3243f6e4655e6ac
yelin04316 <yelin04316@gmail.com>
2026-01-02 21:29:53 +0900

    commit

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,9 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+u$mf8n7-#&c0vyt$_$wf#5ogxk(^h(^pm^3u1iy7*2zr*me^a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
