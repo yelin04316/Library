e054d616a47cdbbb3945364dc2a58d7c6c4173a7
yelin04316 <yelin04316@gmail.com>
2026-01-02 21:39:38 +0900

    commit

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+u$mf8n7-#&c0vyt$_$wf#5ogxk(^h(^pm^3u1iy7*2zr*me^a'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
